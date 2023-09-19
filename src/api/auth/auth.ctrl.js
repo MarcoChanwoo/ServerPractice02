bd7bcdf4aaf499a7ce3d167d1be69697b6b02e76
MarcoChanwoo <cwhong8087@gmail.com>
2023-09-20 00:55:25 +0900

    update

--- src/api/auth/auth.ctrl.js
+++ src/api/auth/auth.ctrl.js
@@ -1,5 +1,42 @@
+import Joi from '../../../node_modules/joi/lib/index';
+import User from '../../models/user';
+
+/*
+    POST /api/auth/register
+    {
+        username: marco,
+        password: my123
+    }
+*/
 export const register = async (ctx) => {
-    // 회원가입
+    const schema = Joi.object().keys({
+        username: Joi.string().alphanum().min(3).max(20).required(),
+        password: Joi.string().required(),
+    });
+    const result = schema.validate(ctx.request.body);
+    if (result.error) {
+        ctx.status = 400;
+        ctx.body = result.error;
+        return;
+    }
+
+    const { username, password } = ctx.request.body;
+    try {
+        const exists = await User.findByUsername(username);
+        if (exists) {
+            ctx.status = 409; // Conflict
+            return;
+        }
+
+        const user = new User({
+            username,
+        });
+        await user.setPassword(password);
+        await user.save(); // 데이터베이스에 저장
+        ctx.body = user.serialize();
+    } catch (e) {
+        ctx.throw(500, e);
+    }
 };
 export const login = async (ctx) => {
     // 로그인
--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,11 @@
-// const Router = require('koa-router');
-// const posts = require('./posts');
 import Router from 'koa-router';
 import posts from './posts';
+import auth from './auth/index';
 
 const api = new Router();
 
 api.use('/posts', posts.routes());
+api.use('/auth', auth.routes());
 
 // 라우터 내보내기
 // module.exports = api;
