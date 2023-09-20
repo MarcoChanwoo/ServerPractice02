43f51431458b1cd7f9c1acbb9f306f1eafdda2c4
MarcoChanwoo <cwhong8087@gmail.com>
2023-09-20 10:20:11 +0900

    update

---
+++ src/lib/jwtMiddleware.js
@@ -0,0 +1,14 @@
+import Jwt from 'jsonwebtoken';
+
+const jwtMiddleware = (ctx, next) => {
+    const token = ctx.cookies.get('access_token');
+    if (!token) return next();
+    try {
+        const decoded = Jwt.verify(token, process.env.JWT_SECRET);
+        console.log(decoded);
+        return next();
+    } catch (e) {
+        return next();
+    }
+};
+export default jwtMiddleware;
