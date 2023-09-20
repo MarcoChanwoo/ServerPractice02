f5ec0736dbdf9b4cfab9d7802e1c78cabaf7a573
MarcoChanwoo <cwhong8087@gmail.com>
2023-09-20 11:15:04 +0900

    update

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -25,6 +25,15 @@ export const getPostById = async (ctx, next) => {
     return next();
 };
 
+export const checkOwnPost = (ctx, next) => {
+    const { user, post } = ctx.state;
+    if (post.user._id.toString() !== user._id) {
+        ctx.status = 403;
+        return;
+    }
+    return next();
+};
+
 /*
     POST /api/posts
     {
